886fe6ed6582eb086ceed6178599fd9e8e2fb1b4
Y0SH1M4S73R <legoboyo@earthlink.net>
2026-06-03 01:15:45 -0400

    shamelessly copies rust-g's `install_byond`.sh

--- scripts/install_byond.sh
+++ scripts/install_byond.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
+# Detect OS
+# In GitHub Actions Windows runners with Git Bash, OSTYPE is "msys"
+# On Windows 2025+ runners, bash may resolve to WSL where OSTYPE is "linux-gnu",
+# but OS=Windows_NT is still inherited from the Windows environment.
+if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "win32" || "${OS:-}" == "Windows_NT" ]]; then
+    IS_WINDOWS=1
+    BYOND_SUFFIX=""
+else
+    IS_WINDOWS=0
+    BYOND_SUFFIX="_linux"
+fi
+
+if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt 2>/dev/null;
 then
     echo "Using cached directory."
 else
@@ -9,11 +21,24 @@ else
     rm -rf "$HOME/BYOND"
     mkdir -p "$HOME/BYOND"
     cd "$HOME/BYOND"
-    curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+
+    if ! curl --connect-timeout 2 --max-time 8 "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond${BYOND_SUFFIX}.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+        echo "Mirror download failed, falling back to byond.com"
+        if ! curl --connect-timeout 2 --max-time 8 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond${BYOND_SUFFIX}.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+            echo "BYOND download failed too :("
+            exit 1
+        fi
+    fi
+
     unzip byond.zip
     rm byond.zip
-    cd byond
-    make here
+
+    if [ "$IS_WINDOWS" -eq 0 ]; then
+        cd byond
+        make here
+        cd ..
+    fi
+
     echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
     cd ~/
 fi
